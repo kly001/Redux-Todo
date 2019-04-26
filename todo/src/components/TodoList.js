8d0289f895ab71c73047a78619a192bd3f56e5bb
kly001 <karenly1961@gmail.com>
2019-04-26 02:33:29 -0400

    Added form to TodoList.js

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,18 +1,37 @@
 import React from "react";
 import {connect} from "react-redux";
 
-const TodoList = props => {
+class TodoList extends React.Component  {
+    state = {
+        newTodo:"",
+    }
+
+    handleChange = event =>{
+        this.setState({newTodo:event.target.value})
+    }
+    render() {
     return(
         <> 
         <div className ="todo-list">
-            {props.todoList.map(todo => (
+            {this.props.todoList.map(todo => (
                 <h4 key={todo.id}>
             {todo.task}
                 </h4>
             ))}
           </div>  
+          <input
+          type="text"
+          value={this.state.newTodo}
+          onChange={this.handleChange}
+          placeholder="Add New Todo"
+          />
+          <button>Add Todo</button>
+
+
+          
         </>  
     )
+}
 }
 
 const mapStateToProps = state => {
